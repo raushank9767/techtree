ff2bc37c9a2f85b2b4625868a6bb429a079d42a6
raushan <raushan8767@gmail.com>
2018-07-30 23:15:03 +0530

    dd

--- TestApp/src/com/nt/test/FindMax.java
+++ TestApp/src/com/nt/test/FindMax.java
@@ -5,11 +5,11 @@ public class FindMax {
 	public static void main(String[] args) {
 		
 		//string array is created
-		String[] names={"rama","shyama","mona","sona","rita"};
+		String[] names={"rama","shyama","mona","sona",};
 		//iterate over the array
 		for(String name:names){
 			System.out.println(name);
-			System.out.println("how r u doing today buddy");
+			System.out.println("how r u doing today");
 		}
 
 		
