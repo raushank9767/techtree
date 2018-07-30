ee83242452d9dd90327f6f2e5fbea0934685fa6d
raushan <raushan8767@gmail.com>
2018-07-30 23:04:00 +0530

    name is added in string array

--- TestApp/src/com/nt/test/FindMax.java
+++ TestApp/src/com/nt/test/FindMax.java
@@ -5,10 +5,11 @@ public class FindMax {
 	public static void main(String[] args) {
 		
 		//string array is created
-		String[] names={"rama","shyama","mona"};
+		String[] names={"rama","shyama","mona","sona","rita"};
 		//iterate over the array
 		for(int i=0; i<names.length;i++){
 			System.out.println(names[i]);
+			System.out.println("how r u doing today buddy");
 		}
 		
 	}
