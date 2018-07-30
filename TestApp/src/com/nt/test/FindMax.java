bf8a9916409950a8f58af308c4007a7634c94d7a
raushan <raushan8767@gmail.com>
2018-07-30 22:15:59 +0530

    string array is created and iterated over array

--- TestApp/src/com/nt/test/FindMax.java
+++ TestApp/src/com/nt/test/FindMax.java
@@ -1,7 +1,15 @@
 package com.nt.test;
 
 public class FindMax {
+
 	public static void main(String[] args) {
+		
+		//string array is created
+		String[] names={"rama","shyama","mona"};
+		
+		for(int i=0; i<names.length;i++){
+			System.out.println(names[i]);
+		}
 		
 	}
 
