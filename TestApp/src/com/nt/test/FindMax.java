a5c1937351b4ca8ab25d1eec863e0b3fac86988c
raushan <raushan8767@gmail.com>
2018-07-30 23:27:51 +0530

    iteration done over array

--- TestApp/src/com/nt/test/FindMax.java
+++ TestApp/src/com/nt/test/FindMax.java
@@ -6,17 +6,17 @@ public class FindMax {
 		
 		//string array is created
 		String[] names={"rama","shyama","mona","sona",};
-		//iterate over the array
+		//iterate over array
 		for(String name:names){
 			System.out.println(name);
-<<<<<<< HEAD
+
 			//System.out.println("how r u doing today buddy");
-=======
+		}
 
 			
-			//System.out.println("how r u doing today buddy");
+		
 
->>>>>>> branch 'master' of https://github.com/raushank9767/techtree.git
+
 		}
 
 		
@@ -25,6 +25,6 @@ public class FindMax {
 
 		
 	}
-}
+
 
 
